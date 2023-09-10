befbe8768cadd91420b32dc9c10aa439c407914d
Logan Ellis <logan.ellis@onyxhealth.io>
2023-09-10 16:26:46 -0400

    added new member match payloads

--- membermatch/datavalidation.py
+++ membermatch/datavalidation.py
@@ -2,7 +2,7 @@ from datetime import date
 from icecream import ic
 from .settings import FHIR_BASE_URL, CLIENT_ID, CLIENT_SECRET, TENANT, AUTH_URL, SECURE_URL
 from .settings import FHIR_STORE_SENSITIVITY
-from .classes import OperationOutcomeException, DEFAULT_CODE,DEFAULT_STATUS_CODE, DEFAULT_SEVERITY, DEFAULT_DESCRIPTION
+from .classes import OperationOutcomeException, DEFAULT_CODE, DEFAULT_STATUS_CODE, DEFAULT_SEVERITY, DEFAULT_DESCRIPTION
 from requests import request
 from .accesstoken import AccessToken
 import logging
@@ -108,12 +108,13 @@ def evaluate_consent(consent={}, member=""):
     ic(today)
     if "policy" in consent.keys():
         ic("checking consent policy")
-        for i in consent['policy']:
-            ic(consent['policy'])
-            if "uri" in i.keys():
-                ic(i['uri'])
-                if i['uri'] in [REGULAR, SENSITIVE]:
-                    access_mode = i["uri"]
+        access_modes = evaluate_policy(consent['policy'])
+        # for i in consent['policy']:
+        #     ic(consent['policy'])
+        #     if "uri" in i.keys():
+        #         ic(i['uri'])
+        #         if i['uri'] in [REGULAR, SENSITIVE]:
+        #             access_mode = i["uri"]
     if "provision" in consent.keys():
         ic("checking provision")
         if "type" in consent['provision'].keys():
@@ -140,15 +141,19 @@ def evaluate_consent(consent={}, member=""):
         raise OperationOutcomeException(status_code=error['status_code'],
                                         description=error['description'])
 
-    if FHIR_STORE_SENSITIVITY in ["EXCLUDED", "INCLUDED_LABELLED"]:
-        accepted = True
-        ic(FHIR_STORE_SENSITIVITY)
-    else:
-        # FHIR_STORE_SENSITIVITY =  "INCLUDED_NOLABEL"
-        ic(FHIR_STORE_SENSITIVITY)
-        accepted = False
-        if access_mode == REGULAR:
-            accepted = True
+    accepted = fhirstore_can_comply(access_modes)
+    # if FHIR_STORE_SENSITIVITY in ["EXCLUDED", "INCLUDED_LABELLED"]:
+    #     accepted = True
+    #     ic(FHIR_STORE_SENSITIVITY)
+    # else:
+    #     # FHIR_STORE_SENSITIVITY =  "INCLUDED_NOLABEL"
+    #     '''
+    #     If the uri has a tag of #regular andthe FHIR_STORE_SENSITIVITY is INCLUDED_NOLABEL then that means we are unable to differentiate between the sensitive and not sensitive and so we should deny access
+    #     '''
+    #     ic(FHIR_STORE_SENSITIVITY)
+    #     accepted = False
+    #     if access_mode == SENSITIVE:
+    #         accepted = False
     if accepted:
         # Write the Consent record for the member_id
         # it will be needed when an access token is requested
@@ -193,9 +198,9 @@ def call_fhir(calltype="GET", query="", id=""):
     """
     if SECURE_URL:
         access_token = TOKEN.get_token()
-        headers ={"Accept": "application/json",
-                  "Authorization": "Bearer %s" %access_token,
-                  "Content-Type": "application/json"}
+        headers = {"Accept": "application/json",
+                   "Authorization": "Bearer %s" % access_token,
+                   "Content-Type": "application/json"}
     else:
         headers = {"Accept": "application/json",
                    "Content-Type": "application/json"}
@@ -205,8 +210,9 @@ def call_fhir(calltype="GET", query="", id=""):
         resp = response.json()
     except ValueError:
         resp = {}
-    if response.status_code not in [200,201, 204]:
-        logging.info(f"{response.status_code}:Problem with {calltype} call to FHIR Store")
+    if response.status_code not in [200, 201, 204]:
+        logging.info(
+            f"{response.status_code}:Problem with {calltype} call to FHIR Store")
         logging.info(response.content)
     logging.debug(resp)
     return response.status_code, resp
@@ -222,9 +228,9 @@ def write_fhir(calltype="POST", data={}):
     ic("writing FHIR resource")
     if SECURE_URL:
         access_token = TOKEN.get_token()
-        headers ={"Accept": "application/json",
-                  "Authorization": "Bearer %s" %access_token,
-                  "Content-Type": "application/json"}
+        headers = {"Accept": "application/json",
+                   "Authorization": "Bearer %s" % access_token,
+                   "Content-Type": "application/json"}
     else:
         headers = {"Accept": "application/json",
                    "Content-Type": "application/json"}
@@ -239,17 +245,18 @@ def write_fhir(calltype="POST", data={}):
                  'code': DEFAULT_CODE, 'severity': DEFAULT_SEVERITY,
                  'description': "resourceType not specified"}
 
-        raise OperationOutcomeException(status_code=error['status_code'],
-                                        description=error['description'])
+        raise OperationOutcomeException(
+            status_code=error['status_code'], description=error['description'])
     ic(data)
     response = requests.post(url, json=data, headers=headers)
     try:
         resp = response.json()
-    except ValueError:
-        resp = {}
+    except ValueError as ve:
+        resp = { "error": ve }
     ic(resp)
-    if response.status_code not in [200,201, 204]:
-        logging.info(f"{response.status_code}:Problem with {calltype} call to FHIR Store")
+    if response.status_code not in [200, 201, 204]:
+        logging.info(
+            f"{response.status_code}:Problem with {calltype} call to FHIR Store")
         logging.info(response.content)
     logging.debug(resp)
 
@@ -291,9 +298,9 @@ def get_metadata():
 
     if SECURE_URL:
         access_token = TOKEN.get_token()
-        headers ={"Accept": "application/json",
-                  "Authorization": "Bearer %s" %access_token,
-                  "Content-Type": "application/json"}
+        headers = {"Accept": "application/json",
+                   "Authorization": "Bearer %s" % access_token,
+                   "Content-Type": "application/json"}
     else:
         headers = {"Accept": "application/json",
                    "Content-Type": "application/json"}
@@ -307,9 +314,56 @@ def get_metadata():
     except ValueError:
         resp = {}
     ic(resp)
-    if response.status_code not in [200,201, 204]:
-        logging.info(f"{response.status_code}:Problem with {calltype} call to FHIR Store")
+    if response.status_code not in [200, 201, 204]:
+        logging.info(
+            f"{response.status_code}:Problem with {calltype} call to FHIR Store")
         logging.info(response.content)
     logging.debug(resp)
 
     return response.status_code, resp
+
+
+def evaluate_policy(policy=[]):
+    '''
+        "policy" : [
+            {
+                "uri" : "http://hl7.org/fhir/us/davinci-hrex/StructureDefinition-hrex-consent.html#regular"
+            }
+        ],
+
+    :param policy:
+    :return:
+    '''
+    ic(policy)
+    access_modes = []
+    for i in policy:
+        if "uri" in i.keys():
+            if i['uri'] not in access_modes:
+                access_modes.append(i["uri"])
+
+    return (access_modes)
+
+
+def fhirstore_can_comply(access_modes=[]):
+    '''
+    :param access_modes:
+    :return accepted:
+    '''
+    accepted = False
+    ic(access_modes)
+    if FHIR_STORE_SENSITIVITY in ["EXCLUDED", "INCLUDED_LABELLED"]:
+        # We can deal with sensitive or regular requests
+        # EXCLUDED means the fhirstore has no sensitive data to return
+        accepted = True
+        ic(FHIR_STORE_SENSITIVITY)
+    else:
+        # FHIR_STORE_SENSITIVITY =  "INCLUDED_NOLABEL"
+        # The fhir store has sensitive data but it can't be identified separately from regular data.
+        # Therefore any request to return just regular data will have to be refused.
+        ic(FHIR_STORE_SENSITIVITY)
+        if SENSITIVE in access_modes:
+            accepted = False
+        else:
+            accepted = True
+
+    return (accepted)
